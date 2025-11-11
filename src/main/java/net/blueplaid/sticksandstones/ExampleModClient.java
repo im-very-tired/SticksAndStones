082bc9e7da2c7bbd40790cf590012944168d45e6
attic <atticus.butchart@gmail.com>
2025-11-11 13:34:16 -0800

    innit setup (penis)

--- src/main/java/net/blueplaid/sticksandstones/ExampleModClient.java
+++ src/main/java/net/blueplaid/sticksandstones/ExampleModClient.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.blueplaid.sticksandstones;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
+@Mod(value = ExampleMod.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = ExampleMod.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = ExampleMod.MOD_ID, value = Dist.CLIENT)
 public class ExampleModClient {
     public ExampleModClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
