ab2cd99de22fb9ac9ca7d8395d7834f0bac55098
attic <atticus.butchart@gmail.com>
2025-12-07 14:58:01 -0800

    Bronze added, busted creative menu

--- src/main/java/net/blueplaid/sticksandstones/SticksAndStones.java
+++ src/main/java/net/blueplaid/sticksandstones/SticksAndStones.java
@@ -1,5 +1,7 @@
 package net.blueplaid.sticksandstones;
 
+import net.blueplaid.sticksandstones.item.Moditems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -28,6 +30,8 @@ public class SticksAndStones {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
+        Moditems.register(modEventBus);
+
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
@@ -46,7 +50,9 @@ public class SticksAndStones {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(Moditems.BRONZEINGOT);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
